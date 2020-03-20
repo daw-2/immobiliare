9569ae9631da37e9926cf77b12c2cf7aed8d5f42
Matthieu Mota <matthieumota@gmail.com>
2020-03-20 15:24:16 +0100

    Préparation du controlleur des annonces

--- src/Controller/AnnonceController.php
+++ src/Controller/AnnonceController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AnnonceController extends AbstractController
@@ -13,5 +14,41 @@ class AnnonceController extends AbstractController
     public function index()
     {
         return $this->render('annonce/index.html.twig');
+    }
+
+    /**
+     * @Route("/annonces/{id}", name="annonces_voir")
+     */
+    public function voir($id)
+    {
+        return $this->render('annonce/voir.html.twig', [
+            'id' => $id,
+        ]);
+    }
+
+    /**
+     * @Route("/annonces/creer", name="annonces_creer")
+     */
+    public function creer()
+    {
+        return $this->render('annonce/creer.html.twig');
+    }
+
+    /**
+     * @Route("/annonces/modifier/{id}", name="annonces_modifier")
+     */
+    public function modifier($id)
+    {
+        return $this->render('annonce/modifier.html.twig', [
+            'id' => $id,
+        ]);
+    }
+
+    /**
+     * @Route("/annonces/supprimer/{id}", name="annonces_supprimer")
+     */
+    public function supprimer($id)
+    {
+        return new Response('<body>On supprime '.$id.' et on redirige vers la liste des annonces</body>');
     }
 }
